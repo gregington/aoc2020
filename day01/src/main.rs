c09d20d1809e3d4e60973e050aeb0ab2e0da4087
Greg Tan <greg.tan@firstmode.com>
2024-06-01 20:14:21 +0800

    Day 1,  part 1

---
+++ day01/src/main.rs
@@ -0,0 +1,35 @@
+use std::collections::HashSet;
+use std::io::BufRead;
+use std::{env, io};
+use std::fs::File;
+use std::path::Path;
+
+fn part1() {
+    let lines = read_lines("input.txt").unwrap();
+    let nums_iter = lines.map(|line| line.unwrap().parse::<i32>().unwrap());
+    let nums_set: HashSet<i32> = HashSet::from_iter(nums_iter);
+
+    for num in nums_set.iter()  {
+        if nums_set.contains(&(2020 - num)) {
+            println!("{}", num * (2020 - num));
+            break;
+        }
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let part = args[1].parse::<i32>().unwrap();
+
+    match part {
+        1 => part1(),
+        2 => println!("Part 2"),
+        _ => println!("Invalid part number"),
+    }
+}
