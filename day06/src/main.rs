59bd4dbc97fdb23fad763804253c0cbbb2115586
Greg Tan <greg.tan@firstmode.com>
2024-06-03 13:08:15 +0800

    Day 6, part 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -7,16 +7,18 @@ use std::path::Path;
 use std::vec::Vec;
 
 fn part1(lines: &Vec<String>) {
-    let answer_groups = create_answer_groups(lines);
-    let sum: u64 = answer_groups.iter().map(|x| x.len() as u64).sum();
+    let groups = create_any_answered_yes_groups(lines);
+    let sum: u64 = groups.iter().map(|x| x.len() as u64).sum();
     println!("{sum}");
 }
 
 fn part2(lines: &Vec<String>) {
-    println!("Part 2");
+    let groups = create_all_answered_yes_groups(lines);
+    let sum: u64 = groups.iter().map(|x| x.len() as u64).sum();
+    println!("{sum}");
 }
 
-fn create_answer_groups(lines: &Vec<String>) -> Vec<HashSet<char>> {
+fn create_any_answered_yes_groups(lines: &Vec<String>) -> Vec<HashSet<char>> {
     let mut groups: Vec<HashSet<char>> = Vec::new();
     let mut answers: HashSet<char> = HashSet::new();
 
@@ -34,6 +36,35 @@ fn create_answer_groups(lines: &Vec<String>) -> Vec<HashSet<char>> {
 
     if !&answers.is_empty() {
         groups.push(answers);
+    }
+
+    groups
+}
+
+fn create_all_answered_yes_groups(lines: &Vec<String>) -> Vec<HashSet<char>> {
+    let mut groups: Vec<HashSet<char>> = Vec::new();
+    let mut answers: Option<HashSet<char>> = None;
+
+    for line in lines {
+        if line.trim().is_empty() && answers.is_some() {
+            groups.push(answers.unwrap());
+            answers = None;
+            continue;
+        }
+
+        if answers.is_none()
+        {
+            answers = Some(line.chars().into_iter().collect::<HashSet<char>>());
+            continue;
+        }
+
+        let s: HashSet<char> = HashSet::from_iter(line.chars().into_iter());
+        let intersection = &answers.unwrap().intersection(&s).map(|x| *x).collect::<HashSet<char>>();
+        answers = Some(intersection.clone());
+    }
+
+    if answers.is_some() {
+        groups.push(answers.unwrap());
     }
 
     groups
