2a158d33de0be8017a254ce46178d0b8464c22cf
Greg Tan <greg.tan@firstmode.com>
2024-06-03 19:58:15 +0800

    Day 9, part 2

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, env, fs::File, io::{self, BufRead}, path::Path};
+use std::{cmp::{max, min}, collections::HashSet, env, fs::File, io::{self, BufRead}, path::Path};
 
 fn part1(lines: &Vec<String>, memory: i32) {
     let numbers = read_numbers(lines);
@@ -6,8 +6,12 @@ fn part1(lines: &Vec<String>, memory: i32) {
     println!("{invalid_number}");
 }
 
-fn part2(lines: &Vec<String>) {
-    println!("Part 2");
+fn part2(lines: &Vec<String>, memory: i32) {
+    let numbers = read_numbers(lines);
+    let invalid_number = find_invalid_number(&numbers, memory);
+    let (min, max) = find_contiguous_sum(&numbers, invalid_number);
+    let sum = min + max;
+    println!("{sum}");
 }
 
 fn find_invalid_number(numbers: &[i64], memory: i32) -> i64 {
@@ -37,6 +41,44 @@ fn contains_sum(mem: &HashSet<i64>, num: i64) -> bool {
     false
 }
 
+fn find_contiguous_sum(numbers: &[i64], invalid_number: i64) -> (i64, i64) {
+    for i in 0..numbers.len() {
+        let slice = &numbers[i..];
+
+        let result = find_contiguous_slice(&slice, invalid_number);
+        if result.is_none() {
+            continue;
+        }
+
+        return result.unwrap();
+    }
+
+    panic!("No contiguous sum found");
+}
+
+fn find_contiguous_slice(numbers: &[i64], invalid_number: i64) -> Option<(i64, i64)> {
+    let mut sum:i64 = 0;
+
+    let mut min_num = i64::MAX;
+    let mut max_num = i64::MIN;
+
+    for num in numbers.iter() {
+        min_num = min(*num, min_num);
+        max_num = max(*num, max_num);
+        sum += *num;
+
+        if sum == invalid_number {
+            return Some((min_num, max_num));
+        }
+
+        if sum > invalid_number {
+            return None;
+        }
+    }
+
+    None
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let part = args[1].parse::<i32>().unwrap();
@@ -53,7 +95,7 @@ fn main() {
 
     match part {
         1 => part1(&lines, memory),
-        2 => part2(&lines),
+        2 => part2(&lines, memory),
         _ => println!("Invalid part number"),
     }
 }
