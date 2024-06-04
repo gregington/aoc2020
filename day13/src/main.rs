7ca3928a97fb7c7ea12ef28f56049fdc972c387f
Greg Tan <greg.tan@firstmode.com>
2024-06-04 17:09:49 +0800

    Day 13, part 2.

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -4,10 +4,10 @@ use std::env;
 use std::io::BufRead;
 use std::path::Path;
 
-fn part1(earliest_time: i64, bus_ids: &str) {
+fn part1(earliest_time: i128, bus_ids: &str) {
     let bus_ids = parse_bus_ids(bus_ids);
 
-    let mut earliest_bus_times: Vec<(i64, i64)> = bus_ids.iter()
+    let mut earliest_bus_times: Vec<(i128, i128)> = bus_ids.iter()
         .map(|id| {
             let time_before = (earliest_time / id) * id;
             let earliest_bus_time = if time_before == earliest_time { earliest_time } else { time_before + id };
@@ -20,15 +20,47 @@ fn part1(earliest_time: i64, bus_ids: &str) {
     println!("{product}");
 }
 
-fn part2(earliest_time: i64, bus_ids: &str) {
-    println!("Part 2");
+fn part2(_earliest_time: i128, bus_ids: &str) {
+    let congruences = parse_congruences(&bus_ids);
+
+    let mut solution: i128 = 0;
+    let mut step_size: i128 = 1;
+
+    for c in congruences {
+        for timestep in (solution..i128::MAX).step_by(step_size as usize) {
+            if (timestep + c.a) % c.m == 0 {
+                solution = timestep;
+                step_size *= c.m;
+                break;
+            }
+        }
+    }
+
+    println!("{solution}");
 }
 
-fn parse_bus_ids(bus_ids: &str) -> Vec<i64> {
+fn parse_bus_ids(bus_ids: &str) -> Vec<i128> {
     bus_ids.split(',')
         .filter(|x| *x != "x")
         .map(|x| x.parse().unwrap())
         .collect()
+}
+
+fn parse_congruences(bus_ids: &str) -> Vec<Congruence> {
+    let parts: Vec<&str> = bus_ids.split(',').collect();
+    let mut congruences: Vec<Congruence> = Vec::new();
+
+    for i in 0..parts.len() {
+        if parts[i] == "x" {
+            continue;
+        }
+        congruences.push(Congruence {
+            a: i as i128,
+            m: parts[i].parse().unwrap()
+        });
+    }
+
+    congruences
 }
 
 fn main() {
@@ -43,7 +75,7 @@ fn main() {
     let lines: Vec<String> = read_lines(&filename).unwrap()
         .map(|x| x.unwrap()).collect();
 
-    let earliest_time: i64 = lines[0].parse().unwrap();
+    let earliest_time: i128 = lines[0].parse().unwrap();
     let bus_ids = &lines[1];
 
     match part {
@@ -57,4 +89,9 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+struct Congruence {
+    a: i128,
+    m: i128
 }
