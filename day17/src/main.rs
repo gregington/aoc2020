6a16ce9f3511fc1942729749b0286a91e4a03c0e
Greg Tan <greg.tan@firstmode.com>
2024-06-08 15:44:11 +0800

    Day 17, part 2.

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -8,29 +8,33 @@ use std::env;
 
 fn part1(lines: &Vec<String>) {
     let mut active = parse_active_cubes(lines);
-    active = run_simulation(&active, 6);
-    // print_board(&active);
+    active = run_simulation_3d(&active, 6);
     println!("{}", active.len());
 }
 
 fn part2(lines: &Vec<String>) {
-    println!("Part 2");
+    let active_cubes = parse_active_cubes(lines);
+    let mut active: HashSet<HyperCube> = active_cubes.iter()
+        .map(|c| HyperCube { x: c.x, y: c.y, z: c.z, w: 0})
+        .collect();
+    active = run_simulation_4d(&active, 6);
+    println!("{}", active.len());
 }
 
-fn run_simulation(active: &HashSet<Cube>, count: u32) -> HashSet<Cube> {
+fn run_simulation_3d(active: &HashSet<Cube>, count: u32) -> HashSet<Cube> {
     let mut prev = (*active).clone();
     let mut current: HashSet<Cube> = HashSet::new();
 
     for _ in 0..count {
         current = HashSet::new();
 
-        let bounds = find_bounds(&prev);
+        let bounds = find_bounds_3d(&prev);
 
         for x in bounds.x.to_owned() {
             for y in bounds.y.to_owned() {
                 for z in bounds.z.to_owned() {
                     let cube = Cube { x, y, z };
-                    let neighbors_active = count_active_neighbors(&prev, &cube);
+                    let neighbors_active = count_active_neighbors_3d(&prev, &cube);
 
                     if prev.contains(&cube) {
                         if neighbors_active == 2 || neighbors_active == 3 {
@@ -51,7 +55,44 @@ fn run_simulation(active: &HashSet<Cube>, count: u32) -> HashSet<Cube> {
     current
 }
 
-fn find_bounds(active: &HashSet<Cube>) -> Bounds {
+fn run_simulation_4d(active: &HashSet<HyperCube>, count: u32) -> HashSet<HyperCube> {
+    let mut prev = (*active).clone();
+    let mut current: HashSet<HyperCube> = HashSet::new();
+
+    for _ in 0..count {
+        current = HashSet::new();
+
+        let bounds = find_bounds_4d(&prev);
+
+        for x in bounds.x.to_owned() {
+            for y in bounds.y.to_owned() {
+                for z in bounds.z.to_owned() {
+                    for w in bounds.w.to_owned() {
+                        let cube = HyperCube { x, y, z, w };
+                        let neighbors_active = count_active_neighbors_4d(&prev, &cube);
+
+                        if prev.contains(&cube) {
+                            if neighbors_active == 2 || neighbors_active == 3 {
+                                current.insert(cube);
+                            }
+                        } else {
+                            if neighbors_active == 3 {
+                                current.insert(cube);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        prev = current.to_owned();
+    }
+
+    current
+}
+
+
+fn find_bounds_3d(active: &HashSet<Cube>) -> Bounds {
     let min_x = active.iter().map(|cube| cube.x).min().unwrap();
     let max_x = active.iter().map(|cube| cube.x).max().unwrap();
     let min_y = active.iter().map(|cube| cube.y).min().unwrap();
@@ -66,7 +107,26 @@ fn find_bounds(active: &HashSet<Cube>) -> Bounds {
     }
 }
 
-fn count_active_neighbors(active: &HashSet<Cube>, cube: &Cube) -> i32 {
+fn find_bounds_4d(active: &HashSet<HyperCube>) -> HyperBounds {
+    let min_x = active.iter().map(|cube| cube.x).min().unwrap();
+    let max_x = active.iter().map(|cube| cube.x).max().unwrap();
+    let min_y = active.iter().map(|cube| cube.y).min().unwrap();
+    let max_y = active.iter().map(|cube| cube.y).max().unwrap();
+    let min_z = active.iter().map(|cube| cube.z).min().unwrap();
+    let max_z = active.iter().map(|cube| cube.z).max().unwrap();
+    let min_w = active.iter().map(|cube| cube.w).min().unwrap();
+    let max_w = active.iter().map(|cube| cube.w).max().unwrap();
+
+    HyperBounds {
+        x: min_x - 1..=max_x + 1,
+        y: min_y - 1..=max_y + 1,
+        z: min_z - 1..=max_z + 1,
+        w: min_w - 1..=max_w + 1
+    }
+}
+
+
+fn count_active_neighbors_3d(active: &HashSet<Cube>, cube: &Cube) -> i32 {
     let mut active_count = 0;
 
     for x in cube.x -1..=cube.x + 1 {
@@ -78,6 +138,28 @@ fn count_active_neighbors(active: &HashSet<Cube>, cube: &Cube) -> i32 {
                 }
                 if active.contains(&neighbor) {
                     active_count += 1;
+                }
+            }
+        }
+    }
+
+    active_count
+}
+
+fn count_active_neighbors_4d(active: &HashSet<HyperCube>, hypercube: &HyperCube) -> i32 {
+    let mut active_count = 0;
+
+    for x in hypercube.x -1..=hypercube.x + 1 {
+        for y in hypercube.y -1..=hypercube.y + 1 {
+            for z in hypercube.z -1..=hypercube.z + 1 {
+                for w in hypercube.w - 1..=hypercube.w + 1 {
+                    let neighbor = HyperCube {x, y, z, w};
+                    if *hypercube == neighbor {
+                        continue;
+                    }
+                    if active.contains(&neighbor) {
+                        active_count += 1;
+                    }
                 }
             }
         }
@@ -111,29 +193,6 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn print_board(active: &HashSet<Cube>) {
-    let min_x = active.iter().map(|cube| cube.x).min().unwrap();
-    let max_x = active.iter().map(|cube| cube.x).max().unwrap();
-    let min_y = active.iter().map(|cube| cube.y).min().unwrap();
-    let max_y = active.iter().map(|cube| cube.y).max().unwrap();
-    let min_z = active.iter().map(|cube| cube.z).min().unwrap();
-    let max_z = active.iter().map(|cube| cube.z).max().unwrap();
-
-    for z in min_z..=max_z {
-        println!();
-        println!("z={z}");
-
-        for y in min_y..=max_y {
-            for x in min_x..=max_x {
-                let cube = Cube{x, y, z};
-                let c = if active.contains(&cube) { '#' } else { '.' };
-                print!("{c}");
-            }
-            println!();
-        }
-    }
-}
-
 fn parse_active_cubes(lines: &Vec<String>) -> HashSet<Cube> {
     let mut active = HashSet::new();
 
@@ -163,4 +222,19 @@ struct Bounds {
     x: RangeInclusive<i32>,
     y: RangeInclusive<i32>,
     z: RangeInclusive<i32>
+}
+
+#[derive(PartialEq, Eq, Hash, Clone)]
+struct HyperCube {
+    x: i32,
+    y: i32,
+    z: i32,
+    w: i32
+}
+
+struct HyperBounds {
+    x: RangeInclusive<i32>,
+    y: RangeInclusive<i32>,
+    z: RangeInclusive<i32>,
+    w: RangeInclusive<i32>
 }
