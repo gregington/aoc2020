57f46ad2dcc6338f8809bde4b8e641390313ece3
Greg Tan <greg.tan@firstmode.com>
2024-06-09 06:28:56 +0800

    Day 19, part 2.

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use std::collections::HashMap;
 use std::env;
 use std::fs::File;
@@ -6,48 +5,73 @@ use std::io;
 use std::io::BufRead;
 use std::path::Path;
 
-fn part1(rules: &Vec<String>, messages: &Vec<String>) {
-    let possible_messages: HashSet<String> = generate_messages(&rules, "0");
-    let matches = messages.iter().filter(|message| possible_messages.contains(*message)).count();
+use regex::Regex;
+
+fn part1(rules: &HashMap<u32, String>, messages: &Vec<String>) {
+    let pattern = format!("^{}$", &generate_regex_pattern(rules, "0"));
+    let regex = Regex::new(&pattern).unwrap();
+
+    let matches = messages.iter().filter(|message| regex.is_match(*message)).count();
     println!("{matches}");
 }
 
-fn part2(rules: &Vec<String>, messages: &Vec<String>) {
-    println!("Part 2");
+fn part2(rules: &HashMap<u32, String>, messages: &Vec<String>) {
+    let mut new_rules = rules.clone();
+    
+    let rule8_reps = 5;
+    let mut rule8 = String::new();
+    for i in 0..rule8_reps {
+        for _ in 0..=i {
+            rule8.push_str("42 ")
+        }
+        if i != rule8_reps - 1 {
+            rule8.push_str("| ");
+        }
+    }
+    new_rules.insert(8, rule8);
+
+    let rule11_reps = 4;
+    let mut rule11 = String::new();
+    for i in 0..rule11_reps {
+        for _ in 0..=i {
+            rule11.push_str("42 ")
+        }
+        for _ in 0..=i {
+            rule11.push_str("31 ")
+        }
+        if i != rule11_reps - 1 {
+            rule11.push_str("| ")
+        }
+    }
+    new_rules.insert(11, rule11);
+
+    let pattern = format!("^{}$", &generate_regex_pattern(&new_rules, "0"));
+    let regex = Regex::new(&pattern).unwrap();
+
+    let matches = messages.iter().filter(|message| regex.is_match(*message)).count();
+    println!("{matches}");
 }
 
-fn generate_messages(rules: &Vec<String>, rule_expr: &str) -> HashSet<String> {
+fn generate_regex_pattern(rules: &HashMap<u32, String>, rule_expr: &str) -> String {
+    let rule_expr = rule_expr.trim();
     if rule_expr.starts_with('"') {
-        let val = rule_expr[1..rule_expr.len() - 1].to_owned();
-        return HashSet::from([val]);
+        return rule_expr[1..rule_expr.len() - 1].to_owned();
     } else if rule_expr.contains('|') {
-        let mut iter = rule_expr.split('|');
-        let lhs = iter.next().unwrap().trim();
-        let rhs = iter.next().unwrap().trim();
-
-        let mut results = HashSet::new();
-        results.extend(generate_messages(rules, lhs));
-        results.extend(generate_messages(rules, rhs));
-
-        return results;
+        let sub_patterns = rule_expr.split("|")
+            .map(|x| generate_regex_pattern(rules, x))
+            .collect::<Vec<String>>()
+            .join("|");
+        return format!("({sub_patterns})");
     } else if rule_expr.contains(' ') {
-        let mut results: HashSet<String> = HashSet::from(["".to_owned()]);
-
+        let mut results = String::from("");
         for subrule in rule_expr.split(' ') {
-            let part_results = generate_messages(rules, subrule);
-
-            let mut new_results = HashSet::new();
-            for prev_result in results {
-                for part_result in &part_results {
-                    new_results.insert(format!("{prev_result}{part_result}"));
-                }
-            }
-            results = new_results;
-        }
+            let part_results = generate_regex_pattern(&rules, subrule);
+            results = format!("{results}{part_results}");
+        }        
         return results;
     } else {
-        let new_rule_expr = &rules[rule_expr.parse::<usize>().unwrap()];
-        return generate_messages(rules, new_rule_expr);
+        let new_rule_expr = &rules[&rule_expr.parse::<u32>().unwrap()];
+        return generate_regex_pattern(rules, new_rule_expr);
     }
 }
 
@@ -72,7 +96,7 @@ fn main() {
     }
 }
 
-fn parse_input(lines: &Vec<String>) -> (Vec<String>, Vec<String>) {
+fn parse_input(lines: &Vec<String>) -> (HashMap<u32, String>, Vec<String>) {
     let rule_lines: Vec<String> = lines.iter().take_while(|s| s.contains(':'))
         .map(|x| x.to_owned())
         .collect();
@@ -86,22 +110,15 @@ fn parse_input(lines: &Vec<String>) -> (Vec<String>, Vec<String>) {
     (rules, message_lines)
 }
 
-fn parse_rules(lines: &Vec<String>) -> Vec<String> {
-    let rule_hashmap: HashMap<u32, &str> = lines.iter()
+fn parse_rules(lines: &Vec<String>) -> HashMap<u32, String> {
+    lines.iter()
         .map(|x| {
             let idx = x.find(':').unwrap();
             let rule_num: u32 = x[..idx].parse().unwrap();
-            let rule = &x[idx+2..];
+            let rule = x[idx+2..].to_owned();
             (rule_num, rule)
         })
-        .collect();
-
-    let max_rule_num = rule_hashmap.keys().max().unwrap();
-    let mut results = Vec::new();
-    for i in 0..=*max_rule_num {
-        results.push(rule_hashmap[&i].to_owned());
-    }
-    results
+        .collect()
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
